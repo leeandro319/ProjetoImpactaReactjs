c26530ea53993c5d7b35fd8b907df94356f19e2f
Leandro <leandro_319@yahoo.com.br>
2020-05-01 16:44:52 -0300

    Projeto Completo
    App ok, Server ok, site ok

---
+++ app-cursos/screens/HomeScreen.js
@@ -0,0 +1,35 @@
+import * as React from 'react';
+import { StyleSheet, View, Text, Button } from 'react-native';
+
+export default function HomeScreen(props) {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.texto}>Página inicial</Text>
+      <View style={styles.botao}>
+        <Button
+          title="Lista de Cursos"
+          onPress={() => props.navigation.navigate('Cursos')} />
+        <Button
+          title="Envie seu contato"
+          onPress={() => props.navigation.navigate('Contato')} />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  texto: {
+    fontSize: 20
+  },
+  botao: {
+    width: '100%',
+    height: '30%',
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+});
--- react-hello/src/Lista.js
+++
@@ -1,3 +0,0 @@
-import React from 'react';
-
-export const Lista = props => <li>{props.texto}</li>
